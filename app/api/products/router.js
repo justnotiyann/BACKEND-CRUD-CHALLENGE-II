d4040796d5831a0786871d15c96dc7aedec6e175
bang_iyan <muhammadfitrian0712@gmail.com>
2022-12-06 00:34:42 +0700

    membuat data

--- app/api/products/router.js
+++ app/api/products/router.js
@@ -5,5 +5,6 @@ router.get("/", controller.getAllProducts);
 router.post("/add", controller.createProduct);
 router.post("/update/:id", controller.updateProduct);
 router.get("/delete/:id", controller.deleteProduct);
+router.get("/search/:id", controller.searchProducts);
 
 module.exports = router;
